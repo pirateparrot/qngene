fb4c79d57781f5d02671a81da7ca578fc5f99b47
pirateparrot <diemalvarez@gmail.com>
2016-10-28 22:11:01 -0400

    Scripts to convert .gen to tsv
    gentotsv.py -> takes .gen/.sample files and converts them into IND, SAM and SNP tsv files
    export.py -> generates pre-Adam tsv files. Allow to generate genotype files either from Advance or .gen files.
    config.py -> has default values for above  mentioned scripts
    utils.py -> shared functions among all python scripts.

---
+++ Export_gen_files/config.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+#title           :config.py
+#description     :Config file
+#author          :Diego Alvarez
+#date            :2016-06-05
+#python_version  :3.5  
+#==============================================================================
+
+##############################
+#Replace with your own values
+##############################
+
+#Default values to be used in gentotsv.py
+source_dir_oxford = "/media/sf_E_DRIVE/PFE/TestMigrationOxford/Data_sample_15/"
+source_file_type_oxford = ".gz"
+destination_dir_oxford = "/media/sf_E_DRIVE/PFE/TestMigrationOxford/gentotsv_15/"
+destination_file_type_oxford = ".tsv"
+sample_file_oxford = "ADVANCE_5.0_6.0.sample"
+sample_file_format_oxford = "IMPUTE2"
+
+
+#To be used in utils.py
+log_file_destination = "/media/sf_E_DRIVE/PFE/TestMigrationOxford/qngene_logs/"
+
+#To be used in export.py
+source_converted_oxford = "/media/sf_E_DRIVE/PFE/TestMigrationOxford/gentotsv_15/"
+imputed_genotype_version = "2i"
